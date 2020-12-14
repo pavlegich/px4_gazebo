7d8ad9fe7700d0067cb4fc2b39c98c8464a7ab41
pavlegich <p.gorodyansky@gmail.com>
2020-12-14 13:52:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
-from trollius import From
-import pygazebo
+import asyncio
+from mavsdk import System
 
-manager = pygazebo.Manager('192.168.1.102', 11345)
-print(manager)
+async def run():
+
+	drone = System()
+	await drone.connect(system_address="udp://:14540")
+
+	print("Waiting for drone to connect...")
+	async for state in drone.core.connection_state():
+		if state.is_connected:
+		print(f"Drone discovered with UUID: {state.uuid}")
+		break
+
+	print("Waiting for drone to have a global position estimate...")
+	async for health in drone.telemetry.health():
+		if health.is_global_position_ok:
+		print("Global position estimate ok")
+		break
+
+	print("-- Arming")
+	await drone.action.arm()
+
+	print("-- Taking off")
+	await drone.action.takeoff()
+
+	await asyncio.sleep(5)
+
+	print("-- Landing")
+	await drone.action.land()
+
+if __name__ == "__main__":
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(run())
