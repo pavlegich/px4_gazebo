511d496353cca533d7702689e8d444cd1a612299
pavlegich <p.gorodyansky@gmail.com>
2020-12-15 21:18:48 +0300

    Update dronekit_app.py

--- dronekit_app.py
+++ dronekit_app.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 print('Connecting to vehicle on: %s' % args.connect)
 vehicle = connect(args.connect, baud=57600, wait_ready=True)
 
-aTargetAltitude = 3
+aTargetAltitude = 30
 
 # Display basic vehicle state
 print(" Type: %s" % vehicle._vehicle_type)
