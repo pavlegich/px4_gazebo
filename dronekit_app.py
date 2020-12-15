f02cfdb877f1cd25c57a97bcaed3c53454fc423e
pavlegich <p.gorodyansky@gmail.com>
2020-12-15 20:56:00 +0300

    Update dronekit_app.py

--- dronekit_app.py
+++ dronekit_app.py
@@ -7,6 +7,8 @@ print("Connecting")
 connection_string = '127.0.0.1:14540'
 vehicle = connect(connection_string, wait_ready=True)
 
+aTargetAltitude = 3
+
 # Display basic vehicle state
 print(" Type: %s" % vehicle._vehicle_type)
 print(" Armed: %s" % vehicle.armed)
@@ -15,12 +17,37 @@ print(" GPS: %s" % vehicle.gps_0)
 print(" Alt: %s" % vehicle.location.global_relative_frame.alt)
 
 # Arm vehicle
-print("Motors arming...")
+print("Arming motors")
+# Copter should arm in GUIDED mode
+vehicle.mode = VehicleMode("GUIDED")
 vehicle.armed = True
+while not vehicle.armed:
+    print " Waiting for arming..."
+    time.sleep(1)
 
+print("Taking off!")
+
+vehicle.simple_takeoff(aTargetAltitude)
+
+ # Check that vehicle has reached takeoff altitude
+while True:
+	print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+	#Break and return from function just below target altitude.        
+	if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
+		print("Reached target altitude")
+		break
+	time.sleep(1)
+
+print("Take off complete")
 
 time.sleep(10)
+
+print("Now let's land")
+vehicle.mode = VehicleMode("LAND")
 
 # Disarm vehicle
 print("Motors disarming...")
 vehicle.armed = False
+
+# Close vehicle object
+vehicle.close()
