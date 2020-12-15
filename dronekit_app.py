d5e672a55c7873a5d3738e18bb0bc5911c7701eb
pavlegich <p.gorodyansky@gmail.com>
2020-12-15 23:23:37 +0300

    Update dronekit_app.py

--- dronekit_app.py
+++ dronekit_app.py
@@ -2,7 +2,7 @@ from dronekit import connect, Command, LocationGlobal, VehicleMode
 from pymavlink import mavutil
 import time, sys, argparse, math
 
-vehicle = mavutil.mavlink_connection('udp:0.0.0.0:14550')
+vehicle = mavutil.mavlink_connection('udp:127.0.0.1:14550')
 
 vehicle.arducopter_arm()
 
