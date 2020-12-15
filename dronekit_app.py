6f597430a92e7bfbb86277e3fda599401ef5f6d7
pavlegich <p.gorodyansky@gmail.com>
2020-12-15 23:42:57 +0300

    Update dronekit_app.py

--- dronekit_app.py
+++ dronekit_app.py
@@ -7,7 +7,7 @@ vehicle = mavutil.mavlink_connection('udpin:localhost:14550')
 altitude = 3
 
 vehicle.mav.command_long_send(
-	vehicle.settings.target_system,  # target_system
+	vehicle.target_system,  # target_system
 	mavutil.mavlink.MAV_COMP_ID_SYSTEM_CONTROL, # target_component
 	mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, # command
 	0, # confirmation
