9b9cb271cac2718af41ab8ea45804bddb4e52a0f
pavlegich <p.gorodyansky@gmail.com>
2020-12-15 18:37:11 +0300

    Update dronekit_app.py

--- dronekit_app.py
+++ dronekit_app.py
@@ -8,8 +8,8 @@ connection_string = '127.0.0.1:14540'
 vehicle = connect(connection_string, wait_ready=True)
 
 # Display basic vehicle state
-print " Type: %s" % vehicle._vehicle_type
-print " Armed: %s" % vehicle.armed
-print " System status: %s" % vehicle.system_status.state
-print " GPS: %s" % vehicle.gps_0
-print " Alt: %s" % vehicle.location.global_relative_frame.alt
+print(" Type: %s" % vehicle._vehicle_type)
+print(" Armed: %s" % vehicle.armed)
+print(" System status: %s" % vehicle.system_status.state)
+print(" GPS: %s" % vehicle.gps_0)
+print(" Alt: %s" % vehicle.location.global_relative_frame.alt)
