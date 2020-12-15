e1f7e2960a8ba29afccc08e63e95ac07fcb1895f
pavlegich <p.gorodyansky@gmail.com>
2020-12-15 23:57:38 +0300

    Update dronekit_app.py

--- dronekit_app.py
+++ dronekit_app.py
@@ -1,6 +1,7 @@
-from dronekit import connect, Command, LocationGlobal, VehicleMode
 from pymavlink import mavutil
-import time, sys, argparse, math
+import time
+from MAVProxy.modules.lib import mp_module
+
 
 vehicle = mavutil.mavlink_connection('udpin:localhost:14550')
 
@@ -10,12 +11,5 @@ vehicle.mav.command_long_send(
 	vehicle.target_system,  # target_system
 	mavutil.mavlink.MAV_COMP_ID_SYSTEM_CONTROL, # target_component
 	mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, # command
-	0, # confirmation
-	0, # param1
-	0, # param2
-	0, # param3
-	0, # param4
-	0, # param5
-	0, # param6
-	altitude) # param7
+	0,0,0,0,0,0,0,altitude)
 
