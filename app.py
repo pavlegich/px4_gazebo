266a71efe681c861075e7d0d576e23f845e99000
Pavel <p.gorodyansky@gmail.com>
2021-03-31 00:32:42 +0300

    try to add camera capture

--- app.py
+++ app.py
@@ -37,24 +37,6 @@ def arducopter_takeoff(vehicle, coordinates, altitude):
 		0,0,0,0,0,coordinates[0][0],coordinates[0][1],
 		coordinates[0][2]+altitude)
 
-# Переместиться
-def arducopter_move(vehicle, coordinates, altitude):
-	print("---Move")
-	vehicle.mav.command_long_send(
-		vehicle.target_system,
-		vehicle.target_component,
-		mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-		0,0,0,0,0,coordinates[0][0]+0.00001,coordinates[0][1]+0.00001,
-		coordinates[0][2]+altitude)
-
-def arducopter_hold(vehicle):
-	print("---Hold")
-	vehicle.mav.command_long_send(
-		vehicle.target_system,
-		vehicle.target_component,
-		mavutil.mavlink.MAV_CMD_NAV_DELAY,
-		0,0,0,0,0,0,0,0)
-
 # Текущая локация
 def current_location(vehicle):
 	vehicle.wait_heartbeat()
@@ -122,8 +104,8 @@ def gps_check(vehicle):
 async def run():
 
 	gps0 = [[0, 0, 0]] # Начальные координаты
-	altitude = 7 # Высота взлета
-	altitude_goal = 5
+	altitude = 6 # Высота взлета
+	altitude_goal = 3
 
 	# Соединение с БВС, получение начальных координат
 	vehicle = mavutil.mavlink_connection('udpin:localhost:14540')
@@ -145,11 +127,16 @@ async def run():
 	arducopter_takeoff(vehicle, gps0, altitude) # Взлет
 	
 	wait_for_height(vehicle, gps0, altitude_goal) # Текущее положение
-	time.sleep(0.5)
+	time.sleep(0.3)
 	vehicle.set_mode_apm(2)
+	# vehicle.mav.command_long_send(
+	# 	vehicle.target_system,
+	# 	vehicle.target_component,
+	# 	mavutil.mavlink.MAV_CMD_IMAGE_START_CAPTURE,
+	# 	0,0,0,0,0,0,0,0)
+	
 	print("current_location:", [round(v,2) for v in current_location(vehicle)])
 	# arducopter_move(vehicle, gps0, altitude)
-	# arducopter_hold(vehicle)
 
 	t1 = time.time()
 	interval = 0
@@ -159,7 +146,6 @@ async def run():
 		interval = t2 - t1
 		time.sleep(0.3)
 
-	# arducopter_hold(vehicle, location)
 	arducopter_land(vehicle) # Посадка
 	wait_for_landing(vehicle, gps0)
 	print("current_location:", [round(v,2) for v in current_location(vehicle)])
